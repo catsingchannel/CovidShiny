331ffdd5229217c1812c0da72cd0edf4a876ec8e
catsingchannel <88886285+catsingchannel@users.noreply.github.com>
2023-08-31 16:12:26 +0800

    Update process.sh
    Update process.sh

--- process/process.sh
+++ process/process.sh
@@ -24,16 +24,12 @@ cat *.fasta > all.fasta
 
 seqkit grep -s -p - all.fasta -v |sed 's/^M$//' |awk '/^>/{f=!d[$1];d[$1]=1}f' > seqkited.fasta
 
-#nucmer --forward -p nucmer ./ref.fa ./seqkited.fasta
-
 nucmer --forward -p nucmer ../data/ref_.fasta ./seqkited.fasta
 
 show-coords -r -c -l nucmer.delta > nucmer.coords
 
 show-snps nucmer.delta -T -l > nucmer.snps
 
-#java -jar ../snps-process-1.0-SNAPSHOT-jar.jar ./nucmer.snps
+./snps-process ./nucmer.snps ../data/gff_c.tsv ../data/ref_.fasta
 
-#./snps-process ./nucmer.snps ../data/gff_c.tsv ../data/ref_.fasta
-
-#Rscript run.R
+Rscript run.R
